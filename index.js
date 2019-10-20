c66a2aa752aa4d24119bbb8da38796651cee09b8
John Winstead <jwinstead@launchdarkly.com>
2019-10-20 22:31:07 +0000

    add asyncly sort

--- index.js
+++ index.js
@@ -1,3 +1,54 @@
-module.exports = exports = async (func) => {
-    return func.then ? await func() : new Promise((done) => { func(done) })
+async function asyncly(func) {
+    return func.then
+      ? await func()
+      : new Promise((done) => {
+          func(done)
+        });
 }
+
+asyncly.sort = async function asyncSort(arr, compareFunc, left = 0, right = arr.length - 1) {
+  if (left < right) {
+    let i = left, j = right, tmp;
+    const pivot = await asyncSortGetPivot(arr[i], arr[i + Math.floor((j - i) / 2)], arr[j], compareFunc);
+    while (true) {
+      while (await compareFunc(arr[i],  pivot) < 0) {
+        i++;
+      }
+      while (await compareFunc(pivot, arr[j]) < 0) {
+        j--;
+      }
+      if (i >= j) {
+        break;
+      }
+      tmp = arr[i];
+      arr[i] = arr[j];
+      arr[j] = tmp;
+
+      i++;
+      j--;
+    }
+    await asyncSort(arr, compareFunc, left, i - 1);
+    await asyncSort(arr, compareFunc, j + 1, right);
+  }
+  return arr;
+}
+
+async function asyncSortGetPivot(prev, self, next, compareFunc) {
+  if (await compareFunc(prev, self) < 0) {
+    if (await compareFunc(self, next) < 0) {
+      return self;
+    } else if (await compareFunc(next, prev) < 0) {
+      return prev;
+    } else {
+      return next;
+    }
+  } else if (await compareFunc(self, next) > 0) {
+    return self;
+  } else if (await compareFunc(next, prev) > 0) {
+    return prev;
+  } else {
+    return next;
+  }
+}
+
+module.exports = exports = asyncly;
